6054a2299d956cfc59b3362ae3f2a5b6b3f27098
zooli <zkiiito@gmail.com>
2018-08-23 23:01:26 +0200

    map playground

---
+++ MapAnimator.js
@@ -0,0 +1,248 @@
+/*global google */
+"use strict";
+var MapAnimator = {
+    map: null,
+    mapdiv: 'map_canvas',
+    marker: null,
+    polyline: null,
+    directionsDisplay: null,
+    endLocation: null,
+    timerHandle: null,
+    defaultStep: 3000,
+    step: this.defaultStep,
+    tick: 100, // milliseconds
+    distance: null,
+    callback: null,
+    animationTriggerEvent: 'tilesloaded',
+
+    initialize: function () {
+        // Create a map and center it on address
+        var myOptions = {
+            zoom: 13,
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+            keyboardShortcuts: false,
+            panControl: false,
+            streetViewControl: false,
+            fullscreenControl: false,
+            zoomControlOptions: {
+                style: google.maps.ZoomControlStyle.SMALL
+            },
+            mapTypeControl: false
+        };
+
+        this.map = new google.maps.Map(document.getElementById(this.mapdiv), myOptions);
+    },
+
+    showStartLocation: function (address, callbackImmediately, callback) {
+        var geocoder = new google.maps.Geocoder(),
+            that = this;
+
+        geocoder.geocode({'address': address}, function (results) {
+            that.map.setCenter(results[0].geometry.location);
+            that.marker = that.createMarker(results[0].geometry.location, "start");
+
+            if (callbackImmediately) {
+                callback();
+            } else {
+                google.maps.event.addListenerOnce(that.map, 'click', function () {
+                    callback();
+                });
+            }
+        });
+    },
+
+    createMarker: function (latlng, label) {
+        var marker = new google.maps.Marker({
+            position: latlng,
+            map: this.map,
+            title: label,
+            zIndex: Math.round(latlng.lat() * -100000) << 5
+        });
+        marker.myname = label;
+
+        return marker;
+    },
+
+    showRoute: function (routeParams, callback) {
+        var that = this;
+        this.callback = callback;
+        this.step = routeParams.speed || this.defaultStep;
+
+        if (this.timerHandle) {
+            clearTimeout(this.timerHandle);
+            this.timerHandle = null;
+        }
+        if (this.marker) {
+            this.marker.setMap(null);
+        }
+
+        if (this.directionsDisplay) {
+            this.directionsDisplay.setMap(null);
+        }
+
+        if (this.polyline) {
+            this.polyline.setMap(null);
+        }
+
+        this.polyline = new google.maps.Polyline({
+            path: [],
+            strokeColor: '#FF0000',
+            strokeWeight: 3
+        });
+
+        google.maps.event.clearListeners(this.map, 'click');
+
+        this.directionsDisplay = new google.maps.DirectionsRenderer({map: this.map});
+
+        if (routeParams.mode === "FLYING") {
+            this.getFlyingPath(routeParams, function (err) {
+                if (err) {
+                    return callback(err);
+                }
+                that.startAnimation();
+            });
+        } else {
+            this.getDrivingPath(routeParams, function (err) {
+                if (err) {
+                    return callback(err);
+                }
+                that.startAnimation();
+            });
+        }
+    },
+
+    getDrivingPath: function (routeParams, callback) {
+        var directionsService = new google.maps.DirectionsService(),
+            that = this,
+            request = {
+                origin: routeParams.from,
+                destination: routeParams.to,
+                travelMode: routeParams.mode || google.maps.DirectionsTravelMode.DRIVING,
+                waypoints: routeParams.waypoints || [],
+                provideRouteAlternatives: false,
+                optimizeWaypoints: false
+            };
+
+        // Route the directions and pass the response to a
+        // function to create markers for each step.
+        directionsService.route(request, function (response, status) {
+            if (status === google.maps.DirectionsStatus.OK) {
+                that.directionsDisplay.setDirections(response);
+
+                var bounds = new google.maps.LatLngBounds(),
+                    legs = response.routes[0].legs;
+
+                // For each route, display summary information.
+                if (legs.length) {
+                    that.marker = that.createMarker(legs[0].start_location, "start");
+
+                    that.endLocation = {latlng: legs[legs.length - 1].end_location};
+
+                    legs.forEach(function (leg) {
+                        leg.steps.forEach(function (step) {
+                            step.path.forEach(function (p) {
+                                that.polyline.getPath().push(p);
+                                bounds.extend(p);
+                            });
+                        });
+                    });
+                }
+
+                that.polyline.setMap(that.map);
+                that.map.fitBounds(bounds);
+                callback();
+            } else {
+                callback(status);
+            }
+        });
+    },
+
+    getFlyingPath: function (routeParams, callback) {
+        var geocoder = new google.maps.Geocoder(),
+            locations = [],
+            waypoints = routeParams.waypoints || [],
+            that = this,
+            bounds = new google.maps.LatLngBounds(),
+            counter = 0;
+
+        locations.push(routeParams.from);
+        waypoints.forEach(function (waypoint) {
+            locations.push(waypoint.location);
+        });
+        locations.push(routeParams.to);
+
+        locations.forEach(function (location, idx) {
+            geocoder.geocode({'address': location}, (function (idx) {
+                return function (results, status) {
+                    if (status === google.maps.GeocoderStatus.OK) {
+                        that.polyline.getPath().setAt(idx, results[0].geometry.location);
+                        bounds.extend(results[0].geometry.location);
+                        counter += 1;
+
+                        if (counter === locations.length) {
+                            that.marker = that.createMarker(that.polyline.getPath().getAt(0), "start");
+                            that.endLocation = {latlng: that.polyline.getPath().getAt(counter - 1)};
+
+                            that.polyline.setMap(that.map);
+                            that.map.fitBounds(bounds);
+                            callback();
+                        }
+                    } else {
+                        callback(status);
+                    }
+                };
+            }(idx)));
+        });
+    },
+
+    startAnimation: function () {
+        var that = this;
+        this.distance = this.polyline.Distance();
+
+        google.maps.event.addListenerOnce(this.map, this.animationTriggerEvent, function () {
+            if (that.timerHandle === null) {
+                that.animate(that.step);
+            }
+        });
+
+        this.map.setCenter(this.polyline.getPath().getAt(0));
+    },
+
+    animate: function (d) {
+        var that = this,
+            p;
+
+        if (d > this.distance) {
+            this.map.panTo(this.endLocation.latlng);
+            this.marker.setPosition(this.endLocation.latlng);
+
+            if (that.callback) {
+                google.maps.event.addListenerOnce(this.map, 'click', function () {
+                    that.callback();
+                });
+            }
+            return;
+        }
+
+        p = this.polyline.GetPointAtDistance(d);
+
+        this.map.panTo(p);
+        this.marker.setPosition(p);
+        this.timerHandle = setTimeout(
+            function () {
+                that.animate(d + that.step);
+            },
+            this.tick
+        );
+    },
+
+    stopAnimation: function () {
+        if (this.timerHandle) {
+            google.maps.event.clearListeners(this.map, this.animationTriggerEvent);
+            clearTimeout(this.timerHandle);
+            this.timerHandle = null;
+            this.map.panTo(this.endLocation.latlng);
+            this.marker.setPosition(this.endLocation.latlng);
+        }
+    }
+};
---
+++ MapGallery.js
@@ -0,0 +1,246 @@
+/*global MapAnimator, $ */
+"use strict";
+var fadeSpeed = "fast";
+
+var MapGallery = {
+    pos: -1,
+    waypoints: [],
+    slideHolder: $("#gallery"),
+    imgdir: "images/",
+    fullscreen: false,
+    watchHashChange: true,
+
+    initialize: function (waypoints, startLocation) {
+        var that = this;
+
+        if ($("body").data("isflickr")) {
+            if (!$("body").data("ispublic")) {
+                $("#btnFlickrLink").hide();
+                $("#btnFlickr").find("br").hide();
+            }
+
+            var isHd = this.getHdParam();
+
+            $("#btnHd").html("HD <span class='status'>" + (isHd ? "on" : "off") + "</span>").click(function (e) {
+                e.preventDefault();
+                document.location.search = "?hd=" + (isHd ? "0" : "1");
+            });
+
+            $("#btnFlickr").show();
+        }
+
+        this.waypoints = waypoints;
+
+        $(document).keydown(function (e) {
+            if ((e.which === 37) || (e.which === 40)) {
+                e.preventDefault();
+                that.move(-1);
+            } else if ((e.which === 38) || (e.which === 39) || (e.which === 32)) {
+                e.preventDefault();
+                that.move(1);
+            } else if (e.which === 88) { //x delete from waypoints
+                e.preventDefault();
+                that.removeImage();
+            } else if (e.which === 83) { //[s]ave to localStorage
+                that.save();
+            } else if (e.which === 76) { //[l]oad from localStorage
+                that.load();
+            }
+        });
+
+        $("#btnPrev").click(function () {
+            that.move(-1);
+        });
+
+        $("#btnNext, #btnHelper").click(function () {
+            that.move(1);
+        });
+
+        this.slideHolder.on("click", function () {
+            that.move(1);
+        });
+
+        MapAnimator.initialize();
+        this.initStep(startLocation);
+
+        window.onhashchange = function () {
+            if (that.watchHashChange) {
+                that.initStep();
+            }
+            that.watchHashChange = true;
+        };
+    },
+
+    getHdParam: function () {
+        var regex = /[^a-z]*hd=(\d+)/g;
+        var matched = regex.exec(document.location.search);
+
+        if (matched) {
+            return parseInt(matched[1], 10) > 0;
+        }
+
+        return true;
+    },
+
+    initStep: function (startLocation) {
+        var that = this,
+            hashPos;
+
+        this.slideHolder.hide();
+        this.slideHolder.empty();
+
+        hashPos = parseInt(window.location.hash.substr(1), 10);
+
+        if (!isNaN(hashPos) && this.waypoints[hashPos - 1] !== undefined) {
+            this.pos = hashPos - 2;//-1 because of 0.., -1 because we have to move to this.
+        } else {
+            $("#btnHelper").show();
+        }
+
+        startLocation = startLocation || this.getFirstLocation();
+        //if we come by a hash, and the current step is not a map
+        if (hashPos && this.waypoints[this.pos + 1].from === undefined) {
+            startLocation = this.getLastLocation() || startLocation;
+        }
+
+        this.updateBtns();
+
+        MapAnimator.stopAnimation();
+        MapAnimator.showStartLocation(startLocation, that.waypoints[that.pos + 1].from === undefined, function () {
+            that.move(1);
+        });
+    },
+
+    move: function (dir) {
+        var that = this;
+        $("#btnHelper").hide();
+        that.openFullscreen();
+        MapAnimator.stopAnimation();
+
+        var currentSlide = this.slideHolder.find(".gallery");
+
+        that.pos += dir;
+        var next = that.waypoints[that.pos];
+
+        currentSlide.fadeOut(fadeSpeed, function () {
+            currentSlide.remove();
+        });
+
+        if (next) {
+            that.preload(that.pos + 1);
+
+            if (next.from !== undefined) {
+                this.slideHolder.fadeOut(fadeSpeed, function () {
+                        MapAnimator.showRoute(next, function (err) {
+                            if (err) {
+                                console.log(err);
+                            }
+                            that.move(1);
+                        });
+                });
+            } else {
+                var nextSlide = $("<div>").css("background-image", "url('" + that.getImageUrl(that.pos) + "')").addClass("gallery").addClass("fullscreen");
+                this.slideHolder.append(nextSlide);
+                nextSlide.fadeIn(fadeSpeed);
+                this.slideHolder.fadeIn(fadeSpeed);
+            }
+        } else {
+            that.pos -= dir;
+        }
+        that.watchHashChange = false;
+        window.location.hash = (that.pos + 1).toString();
+
+        that.updateBtns();
+    },
+
+    updateBtns: function () {
+        if (this.pos <= 0) {
+            $("#btnPrev").hide();
+            $("#btnNext").addClass("hover");
+        } else {
+            $("#btnPrev").show();
+            $("#btnNext").removeClass("hover");
+        }
+
+        if (this.pos + 1 >= this.waypoints.length) {
+            $("#btnNext").hide();
+        } else {
+            $("#btnNext").show();
+        }
+    },
+
+    preload: function (pos) {
+        if (this.waypoints[pos] && this.waypoints[pos].from === undefined) {
+            var img = new Image();
+            img.src = this.getImageUrl(pos);
+        }
+    },
+
+    getImageUrl: function (pos) {
+        var url = this.waypoints[pos];
+        if (url.substr(0, 4) !== "http") {
+            url = this.imgdir + url;
+        }
+        return url;
+    },
+
+    openFullscreen: function () {
+        if (this.fullscreen) {
+            var divObj = document.documentElement;
+
+            if (divObj.requestFullscreen) {
+                divObj.requestFullscreen();
+            } else if (divObj.msRequestFullscreen) {
+                divObj.msRequestFullscreen();
+            } else if (divObj.mozRequestFullScreen) {
+                divObj.mozRequestFullScreen();
+            } else if (divObj.webkitRequestFullscreen) {
+                divObj.webkitRequestFullscreen();
+            }
+        }
+    },
+
+    getFirstLocation: function () {
+        var startLocation = null;
+        this.waypoints.slice(Math.max(this.pos, 0)).some(function (waypoint) {
+            if (waypoint.from !== undefined) {
+                startLocation = waypoint.from;
+                return true;
+            }
+            return false;
+        });
+
+        return startLocation;
+    },
+
+    getLastLocation: function () {
+        var lastLocation = null;
+        this.waypoints.slice(0, this.pos + 1).reverse().some(function (waypoint) {
+            if (waypoint.to !== undefined) {
+                lastLocation = waypoint.to;
+                return true;
+            }
+            return false;
+        });
+
+        return lastLocation;
+    },
+
+    removeImage: function () {
+        this.waypoints.splice(this.pos, 1);
+        this.move(0);
+    },
+
+    save: function () {
+        var data = JSON.stringify(this.waypoints);
+        localStorage.setItem("MapGallery", data);
+        console.log("MapGallery.initialize(" + data.replace(/,"/g, ",\n\"") + ");");
+    },
+
+    load: function () {
+        if (localStorage.getItem("MapGallery")) {
+            this.waypoints = JSON.parse(localStorage.getItem("MapGallery"));
+            this.move(0);
+        }
+    }
+};
