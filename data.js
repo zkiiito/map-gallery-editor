a5d18157926884f5cd61471455d09aa6104b1ec5
zooli <zkiiito@gmail.com>
2018-08-26 21:33:30 +0200

    layout

---
+++ data.js
@@ -0,0 +1,597 @@
+const data = [
+    {
+        id: 1,
+        filename: 'lol.jpg',
+        path: 'file://data/lol.jpg',
+        exif_date: new Date('2018-01-01 01:11:23'),
+        modified_at: new Date('2018-01-01 01:11:23'),
+        visible: true
+    },
+    {
+        id: 2,
+        filename: 'lol.jpg',
+        path: 'file://data/lol.jpg',
+        exif_date: new Date('2017-01-01 01:11:23'),
+        modified_at: new Date('2017-01-01 01:11:23'),
+        visible: true
+    },
+    {
+        id: 3,
+        filename: 'lol.jpg',
+        path: 'file://data/lol.jpg',
+        exif_date: new Date('2016-01-01 01:11:23'),
+        modified_at: new Date('2016-01-01 01:11:23'),
+        visible: true
+    },
+    {
+        id: 4,
+        from: 'Budapest',
+        to: 'Pusztamagyaród',
+        mode: 'DRIVING',
+        speed: 500
+    },
+    {
+        id: 1,
+        filename: 'lol.jpg',
+        path: 'file://data/lol.jpg',
+        exif_date: new Date('2018-01-01 01:11:23'),
+        modified_at: new Date('2018-01-01 01:11:23'),
+        visible: true
+    },
+    {
+        id: 2,
+        filename: 'lol.jpg',
+        path: 'file://data/lol.jpg',
+        exif_date: new Date('2017-01-01 01:11:23'),
+        modified_at: new Date('2017-01-01 01:11:23'),
+        visible: true
+    },
+    {
+        id: 3,
+        filename: 'lol.jpg',
+        path: 'file://data/lol.jpg',
+        exif_date: new Date('2016-01-01 01:11:23'),
+        modified_at: new Date('2016-01-01 01:11:23'),
+        visible: true
+    },
+    {
+        id: 4,
+        from: 'Budapest',
+        to: 'Pusztamagyaród',
+        mode: 'DRIVING',
+        speed: 500
+    },
+    {
+        id: 1,
+        filename: 'lol.jpg',
+        path: 'file://data/lol.jpg',
+        exif_date: new Date('2018-01-01 01:11:23'),
+        modified_at: new Date('2018-01-01 01:11:23'),
+        visible: true
+    },
+    {
+        id: 2,
+        filename: 'lol.jpg',
+        path: 'file://data/lol.jpg',
+        exif_date: new Date('2017-01-01 01:11:23'),
+        modified_at: new Date('2017-01-01 01:11:23'),
+        visible: true
+    },
+    {
+        id: 3,
+        filename: 'lol.jpg',
+        path: 'file://data/lol.jpg',
+        exif_date: new Date('2016-01-01 01:11:23'),
+        modified_at: new Date('2016-01-01 01:11:23'),
+        visible: true
+    },
+    {
+        id: 4,
+        from: 'Budapest',
+        to: 'Pusztamagyaród',
+        mode: 'DRIVING',
+        speed: 500
+    },
+    {
+        id: 1,
+        filename: 'lol.jpg',
+        path: 'file://data/lol.jpg',
+        exif_date: new Date('2018-01-01 01:11:23'),
+        modified_at: new Date('2018-01-01 01:11:23'),
+        visible: true
+    },
+    {
+        id: 2,
+        filename: 'lol.jpg',
+        path: 'file://data/lol.jpg',
+        exif_date: new Date('2017-01-01 01:11:23'),
+        modified_at: new Date('2017-01-01 01:11:23'),
+        visible: true
+    },
+    {
+        id: 3,
+        filename: 'lol.jpg',
+        path: 'file://data/lol.jpg',
+        exif_date: new Date('2016-01-01 01:11:23'),
+        modified_at: new Date('2016-01-01 01:11:23'),
+        visible: true
+    },
+    {
+        id: 4,
+        from: 'Budapest',
+        to: 'Pusztamagyaród',
+        mode: 'DRIVING',
+        speed: 500
+    },
+    {
+        id: 1,
+        filename: 'lol.jpg',
+        path: 'file://data/lol.jpg',
+        exif_date: new Date('2018-01-01 01:11:23'),
+        modified_at: new Date('2018-01-01 01:11:23'),
+        visible: true
+    },
+    {
+        id: 2,
+        filename: 'lol.jpg',
+        path: 'file://data/lol.jpg',
+        exif_date: new Date('2017-01-01 01:11:23'),
+        modified_at: new Date('2017-01-01 01:11:23'),
+        visible: true
+    },
+    {
+        id: 3,
+        filename: 'lol.jpg',
+        path: 'file://data/lol.jpg',
+        exif_date: new Date('2016-01-01 01:11:23'),
+        modified_at: new Date('2016-01-01 01:11:23'),
+        visible: true
+    },
+    {
+        id: 4,
+        from: 'Budapest',
+        to: 'Pusztamagyaród',
+        mode: 'DRIVING',
+        speed: 500
+    },
+    {
+        id: 1,
+        filename: 'lol.jpg',
+        path: 'file://data/lol.jpg',
+        exif_date: new Date('2018-01-01 01:11:23'),
+        modified_at: new Date('2018-01-01 01:11:23'),
+        visible: true
+    },
+    {
+        id: 2,
+        filename: 'lol.jpg',
+        path: 'file://data/lol.jpg',
+        exif_date: new Date('2017-01-01 01:11:23'),
+        modified_at: new Date('2017-01-01 01:11:23'),
+        visible: true
+    },
+    {
+        id: 3,
+        filename: 'lol.jpg',
+        path: 'file://data/lol.jpg',
+        exif_date: new Date('2016-01-01 01:11:23'),
+        modified_at: new Date('2016-01-01 01:11:23'),
+        visible: true
+    },
+    {
+        id: 4,
+        from: 'Budapest',
+        to: 'Pusztamagyaród',
+        mode: 'DRIVING',
+        speed: 500
+    },
+    {
+        id: 1,
+        filename: 'lol.jpg',
+        path: 'file://data/lol.jpg',
+        exif_date: new Date('2018-01-01 01:11:23'),
+        modified_at: new Date('2018-01-01 01:11:23'),
+        visible: true
+    },
+    {
+        id: 2,
+        filename: 'lol.jpg',
+        path: 'file://data/lol.jpg',
+        exif_date: new Date('2017-01-01 01:11:23'),
+        modified_at: new Date('2017-01-01 01:11:23'),
+        visible: true
+    },
+    {
+        id: 3,
+        filename: 'lol.jpg',
+        path: 'file://data/lol.jpg',
+        exif_date: new Date('2016-01-01 01:11:23'),
+        modified_at: new Date('2016-01-01 01:11:23'),
+        visible: true
+    },
+    {
+        id: 4,
+        from: 'Budapest',
+        to: 'Pusztamagyaród',
+        mode: 'DRIVING',
+        speed: 500
+    },
+    {
+        id: 1,
+        filename: 'lol.jpg',
+        path: 'file://data/lol.jpg',
+        exif_date: new Date('2018-01-01 01:11:23'),
+        modified_at: new Date('2018-01-01 01:11:23'),
+        visible: true
+    },
+    {
+        id: 2,
+        filename: 'lol.jpg',
+        path: 'file://data/lol.jpg',
+        exif_date: new Date('2017-01-01 01:11:23'),
+        modified_at: new Date('2017-01-01 01:11:23'),
+        visible: true
+    },
+    {
+        id: 3,
+        filename: 'lol.jpg',
+        path: 'file://data/lol.jpg',
+        exif_date: new Date('2016-01-01 01:11:23'),
+        modified_at: new Date('2016-01-01 01:11:23'),
+        visible: true
+    },
+    {
+        id: 4,
+        from: 'Budapest',
+        to: 'Pusztamagyaród',
+        mode: 'DRIVING',
+        speed: 500
+    },
+    {
+        id: 1,
+        filename: 'lol.jpg',
+        path: 'file://data/lol.jpg',
+        exif_date: new Date('2018-01-01 01:11:23'),
+        modified_at: new Date('2018-01-01 01:11:23'),
+        visible: true
+    },
+    {
+        id: 2,
+        filename: 'lol.jpg',
+        path: 'file://data/lol.jpg',
+        exif_date: new Date('2017-01-01 01:11:23'),
+        modified_at: new Date('2017-01-01 01:11:23'),
+        visible: true
+    },
+    {
+        id: 3,
+        filename: 'lol.jpg',
+        path: 'file://data/lol.jpg',
+        exif_date: new Date('2016-01-01 01:11:23'),
+        modified_at: new Date('2016-01-01 01:11:23'),
+        visible: true
+    },
+    {
+        id: 4,
+        from: 'Budapest',
+        to: 'Pusztamagyaród',
+        mode: 'DRIVING',
+        speed: 500
+    },
+    {
+        id: 1,
+        filename: 'lol.jpg',
+        path: 'file://data/lol.jpg',
+        exif_date: new Date('2018-01-01 01:11:23'),
+        modified_at: new Date('2018-01-01 01:11:23'),
+        visible: true
+    },
+    {
+        id: 2,
+        filename: 'lol.jpg',
+        path: 'file://data/lol.jpg',
+        exif_date: new Date('2017-01-01 01:11:23'),
+        modified_at: new Date('2017-01-01 01:11:23'),
+        visible: true
+    },
+    {
+        id: 3,
+        filename: 'lol.jpg',
+        path: 'file://data/lol.jpg',
+        exif_date: new Date('2016-01-01 01:11:23'),
+        modified_at: new Date('2016-01-01 01:11:23'),
+        visible: true
+    },
+    {
+        id: 4,
+        from: 'Budapest',
+        to: 'Pusztamagyaród',
+        mode: 'DRIVING',
+        speed: 500
+    },
+    {
+        id: 1,
+        filename: 'lol.jpg',
+        path: 'file://data/lol.jpg',
+        exif_date: new Date('2018-01-01 01:11:23'),
+        modified_at: new Date('2018-01-01 01:11:23'),
+        visible: true
+    },
+    {
+        id: 2,
+        filename: 'lol.jpg',
+        path: 'file://data/lol.jpg',
+        exif_date: new Date('2017-01-01 01:11:23'),
+        modified_at: new Date('2017-01-01 01:11:23'),
+        visible: true
+    },
+    {
+        id: 3,
+        filename: 'lol.jpg',
+        path: 'file://data/lol.jpg',
+        exif_date: new Date('2016-01-01 01:11:23'),
+        modified_at: new Date('2016-01-01 01:11:23'),
+        visible: true
+    },
+    {
+        id: 4,
+        from: 'Budapest',
+        to: 'Pusztamagyaród',
+        mode: 'DRIVING',
+        speed: 500
+    },
+    {
+        id: 1,
+        filename: 'lol.jpg',
+        path: 'file://data/lol.jpg',
+        exif_date: new Date('2018-01-01 01:11:23'),
+        modified_at: new Date('2018-01-01 01:11:23'),
+        visible: true
+    },
+    {
+        id: 2,
+        filename: 'lol.jpg',
+        path: 'file://data/lol.jpg',
+        exif_date: new Date('2017-01-01 01:11:23'),
+        modified_at: new Date('2017-01-01 01:11:23'),
+        visible: true
+    },
+    {
+        id: 3,
+        filename: 'lol.jpg',
+        path: 'file://data/lol.jpg',
+        exif_date: new Date('2016-01-01 01:11:23'),
+        modified_at: new Date('2016-01-01 01:11:23'),
+        visible: true
+    },
+    {
+        id: 4,
+        from: 'Budapest',
+        to: 'Pusztamagyaród',
+        mode: 'DRIVING',
+        speed: 500
+    },
+    {
+        id: 1,
+        filename: 'lol.jpg',
+        path: 'file://data/lol.jpg',
+        exif_date: new Date('2018-01-01 01:11:23'),
+        modified_at: new Date('2018-01-01 01:11:23'),
+        visible: true
+    },
+    {
+        id: 2,
+        filename: 'lol.jpg',
+        path: 'file://data/lol.jpg',
+        exif_date: new Date('2017-01-01 01:11:23'),
+        modified_at: new Date('2017-01-01 01:11:23'),
+        visible: true
+    },
+    {
+        id: 3,
+        filename: 'lol.jpg',
+        path: 'file://data/lol.jpg',
+        exif_date: new Date('2016-01-01 01:11:23'),
+        modified_at: new Date('2016-01-01 01:11:23'),
+        visible: true
+    },
+    {
+        id: 4,
+        from: 'Budapest',
+        to: 'Pusztamagyaród',
+        mode: 'DRIVING',
+        speed: 500
+    },
+    {
+        id: 1,
+        filename: 'lol.jpg',
+        path: 'file://data/lol.jpg',
+        exif_date: new Date('2018-01-01 01:11:23'),
+        modified_at: new Date('2018-01-01 01:11:23'),
+        visible: true
+    },
+    {
+        id: 2,
+        filename: 'lol.jpg',
+        path: 'file://data/lol.jpg',
+        exif_date: new Date('2017-01-01 01:11:23'),
+        modified_at: new Date('2017-01-01 01:11:23'),
+        visible: true
+    },
+    {
+        id: 3,
+        filename: 'lol.jpg',
+        path: 'file://data/lol.jpg',
+        exif_date: new Date('2016-01-01 01:11:23'),
+        modified_at: new Date('2016-01-01 01:11:23'),
+        visible: true
+    },
+    {
+        id: 4,
+        from: 'Budapest',
+        to: 'Pusztamagyaród',
+        mode: 'DRIVING',
+        speed: 500
+    },
+    {
+        id: 1,
+        filename: 'lol.jpg',
+        path: 'file://data/lol.jpg',
+        exif_date: new Date('2018-01-01 01:11:23'),
+        modified_at: new Date('2018-01-01 01:11:23'),
+        visible: true
+    },
+    {
+        id: 2,
+        filename: 'lol.jpg',
+        path: 'file://data/lol.jpg',
+        exif_date: new Date('2017-01-01 01:11:23'),
+        modified_at: new Date('2017-01-01 01:11:23'),
+        visible: true
+    },
+    {
+        id: 3,
+        filename: 'lol.jpg',
+        path: 'file://data/lol.jpg',
+        exif_date: new Date('2016-01-01 01:11:23'),
+        modified_at: new Date('2016-01-01 01:11:23'),
+        visible: true
+    },
+    {
+        id: 4,
+        from: 'Budapest',
+        to: 'Pusztamagyaród',
+        mode: 'DRIVING',
+        speed: 500
+    },
+    {
+        id: 1,
+        filename: 'lol.jpg',
+        path: 'file://data/lol.jpg',
+        exif_date: new Date('2018-01-01 01:11:23'),
+        modified_at: new Date('2018-01-01 01:11:23'),
+        visible: true
+    },
+    {
+        id: 2,
+        filename: 'lol.jpg',
+        path: 'file://data/lol.jpg',
+        exif_date: new Date('2017-01-01 01:11:23'),
+        modified_at: new Date('2017-01-01 01:11:23'),
+        visible: true
+    },
+    {
+        id: 3,
+        filename: 'lol.jpg',
+        path: 'file://data/lol.jpg',
+        exif_date: new Date('2016-01-01 01:11:23'),
+        modified_at: new Date('2016-01-01 01:11:23'),
+        visible: true
+    },
+    {
+        id: 4,
+        from: 'Budapest',
+        to: 'Pusztamagyaród',
+        mode: 'DRIVING',
+        speed: 500
+    },
+    {
+        id: 1,
+        filename: 'lol.jpg',
+        path: 'file://data/lol.jpg',
+        exif_date: new Date('2018-01-01 01:11:23'),
+        modified_at: new Date('2018-01-01 01:11:23'),
+        visible: true
+    },
+    {
+        id: 2,
+        filename: 'lol.jpg',
+        path: 'file://data/lol.jpg',
+        exif_date: new Date('2017-01-01 01:11:23'),
+        modified_at: new Date('2017-01-01 01:11:23'),
+        visible: true
+    },
+    {
+        id: 3,
+        filename: 'lol.jpg',
+        path: 'file://data/lol.jpg',
+        exif_date: new Date('2016-01-01 01:11:23'),
+        modified_at: new Date('2016-01-01 01:11:23'),
+        visible: true
+    },
+    {
+        id: 4,
+        from: 'Budapest',
+        to: 'Pusztamagyaród',
+        mode: 'DRIVING',
+        speed: 500
+    },
+    {
+        id: 1,
+        filename: 'lol.jpg',
+        path: 'file://data/lol.jpg',
+        exif_date: new Date('2018-01-01 01:11:23'),
+        modified_at: new Date('2018-01-01 01:11:23'),
+        visible: true
+    },
+    {
+        id: 2,
+        filename: 'lol.jpg',
+        path: 'file://data/lol.jpg',
+        exif_date: new Date('2017-01-01 01:11:23'),
+        modified_at: new Date('2017-01-01 01:11:23'),
+        visible: true
+    },
+    {
+        id: 3,
+        filename: 'lol.jpg',
+        path: 'file://data/lol.jpg',
+        exif_date: new Date('2016-01-01 01:11:23'),
+        modified_at: new Date('2016-01-01 01:11:23'),
+        visible: true
+    },
+    {
+        id: 4,
+        from: 'Budapest',
+        to: 'Pusztamagyaród',
+        mode: 'DRIVING',
+        speed: 500
+    },
+    {
+        id: 1,
+        filename: 'lol.jpg',
+        path: 'file://data/lol.jpg',
+        exif_date: new Date('2018-01-01 01:11:23'),
+        modified_at: new Date('2018-01-01 01:11:23'),
+        visible: true
+    },
+    {
+        id: 2,
+        filename: 'lol.jpg',
+        path: 'file://data/lol.jpg',
+        exif_date: new Date('2017-01-01 01:11:23'),
+        modified_at: new Date('2017-01-01 01:11:23'),
+        visible: true
+    },
+    {
+        id: 3,
+        filename: 'lol.jpg',
+        path: 'file://data/lol.jpg',
+        exif_date: new Date('2016-01-01 01:11:23'),
+        modified_at: new Date('2016-01-01 01:11:23'),
+        visible: true
+    },
+    {
+        id: 4,
+        from: 'Budapest',
+        to: 'Pusztamagyaród',
+        mode: 'DRIVING',
+        speed: 500
+    },
+
+];
+
+let id = 1;
+data.forEach(el => {
+    el.id = id++;
+})
