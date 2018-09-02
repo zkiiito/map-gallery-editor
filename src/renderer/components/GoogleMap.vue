eaf453c185823d5915968a4d9f38faa31150f8b0
zooli <zkiiito@gmail.com>
2018-09-02 22:55:04 +0200

    webview works

--- src/renderer/components/GoogleMap.vue
+++ src/renderer/components/GoogleMap.vue
@@ -1,60 +1,37 @@
 <template>
-    <div :id="id" class="googleMap">
-        <webview src="static/webview.html" id="webv"></webview>
+    <div class="googleMap">
+        <webview src="static/webview.html" ref="webview"/>
     </div>
 </template>
 
 <script>
-/* global MapAnimator */
-const loadGoogleMapsApi = require('load-google-maps-api');
-const LoadJS = require('load-js');
-const uuidv4 = require('uuid/v4');
-
 export default {
     name: 'GoogleMap',
-    data() {
-        return {
-            id: uuidv4(),
-        };
-    },
     computed: {
         currentSlide() {
             return this.$store.state.currentSlide;
         },
     },
     mounted() {
-        const webview = document.getElementById('webv');
+        const that = this;
 
-        webview.addEventListener('did-stop-loading', () => {
-            this.$nextTick(() => {
-                // MapAnimator.mapdiv = this.id;
-                const webview = document.getElementById('webv');
-                webview.executeJavaScript(`MapAnimator.animationTriggerEvent = 'center_changed'`);
-                webview.executeJavaScript(`MapAnimator.initialize()`);
-            });
-
-            this.$bus.$on('map-showroute', () => {
-                const data = {
-                    from: this.currentSlide.from,
-                    to: this.currentSlide.to,
-                    speed: this.currentSlide.speed,
-                    mode: this.currentSlide.mode,
-                };
-
-                webview.executeJavaScript(`MapAnimator.showRoute(` + JSON.stringify(data) + `, (err) => {
+        this.$refs.webview.addEventListener('did-stop-loading', function webviewLoaded() {
+            that.$refs.webview.removeEventListener('did-stop-loading', webviewLoaded);
+            that.$bus.$on('map-showroute', () => {
+                that.$refs.webview.executeJavaScript(`MapAnimator.showRoute(` + JSON.stringify(that.currentSlide) + `, (err) => {
                     console.log('map-error', err);
                 });`);
             });
         });
     },
     watch: {
-        cdurrentSlide(newSlide, oldSlide) {
+        currentSlide(newSlide, oldSlide) {
             if (!oldSlide || !newSlide || newSlide.id !== oldSlide.id) {
-                MapAnimator.stopAnimation();
+                this.$refs.webview.executeJavaScript(`MapAnimator.stopAnimation();`);
                 if (newSlide && newSlide.from) {
-                    MapAnimator.showRoute(newSlide, (err) => {
-                        this.$bus.$emit('map-error', err);
-                    });
+                    this.$refs.webview.executeJavaScript(`MapAnimator.showRoute(` + JSON.stringify(newSlide) + `, (err) => {
+                        console.log('map-error', err);
+                    });`);
                 }
             }
         },
@@ -63,5 +40,7 @@ export default {
 </script>
 
 <style scoped>
-
+webview {
+    height: 100%;
+}
 </style>
