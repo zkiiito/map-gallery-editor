760d58acdfcee92dd848efc863c62abf0ed0ccfd
Feher Zoltan <feher.zoltan@ingatlan.com>
2018-09-06 14:54:50 +0200

    electron-store in webview

--- static/webview-preload.js
+++ static/webview-preload.js
@@ -1,9 +1,11 @@
 const { ipcRenderer } = require('electron');
+const SettingsStore = require('electron-store');
+const settingsStore = new SettingsStore();
 
 global.sendError = (err) => {
     ipcRenderer.sendToHost(err);
 };
 
-global.googleMapsApiKey = 'AIzaSyBxibPU_2mMsI8c5o0wVeG6uBnxps0c6wE';
+global.googleMapsApiKey = settingsStore.get('googleMapsApiKey', null);
 global.loadGoogleMapsApi = require('load-google-maps-api');
 global.loadJS = require('load-js');
